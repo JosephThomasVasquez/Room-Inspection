57cf6fc11646564d85a09791f3fc1596873dd32c
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2020-03-06 05:00:45 -0800

    Added default rooms to database and rendered to the ejs template

--- app.js
+++ app.js
@@ -10,12 +10,9 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-//mongoose.connect("mongodb://localhost:27017/roomsDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/roomsDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
-// const loginForm = document.getElementById('login-form');
-// const userLoginInput = document.getElementById('user-login');
-// const userPasswordInput = document.getElementById('user-password');
-// const loginBtn = document.querySelector('.login-btn');
+
 
 const users = [
     {
@@ -29,20 +26,27 @@ const users = [
 ];
 
 
-
-// Building Schema
+// Room Schema
 const roomSchema = new mongoose.Schema({
     name: String,
     floor: String,
     building: String
 });
 
+// Building Schema
+const buildingSchema = new mongoose.Schema({
+    buildingName: String,
+    rooms: [roomSchema]
+})
+
 const Room = mongoose.model('Room', roomSchema);
+const Building = mongoose.model('Building', buildingSchema);
+
 
 const room1 = new Room({
     name: 'The Beach',
     floor: '1F',
-    building: 'HQ'
+    building: 'Central Building'
 });
 
 const room2 = new Room({
@@ -65,8 +69,45 @@ const room4 = new Room({
 
 const defaultRooms = [room1, room2, room3, room4];
 
+const building = new Building({
+    buildingName: 'Main Building',
+    rooms: defaultRooms
+});
+
+Building.find({}, function (err, foundBuilding) {
+
+    if (!foundBuilding) {
+        building.save();
+        res.redirect('/');
+    };
+});
+
+
+// Find rooms in database and push to database if none found
 app.get('/', function (req, res) {
-    res.render('building', {buildingTitle: defaultRooms[0].building, roomList: defaultRooms })
+
+    Room.find({}, function (err, foundRooms) {
+        
+        if (foundRooms.length === 0) {
+           
+            Room.insertMany(defaultRooms, function (err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log('Saved defaultRooms to database!');
+                }
+            });
+            res.redirect('/');
+        } else {
+            res.render('building', {buildingTitle: "Main building", roomList: defaultRooms });
+        };
+    });
+});
+
+app.post('/', function(req, res) {
+
+    //const roomName = req.body.room;
+
 });
 
 let port = process.env.PORT;
@@ -80,6 +121,11 @@ if (port == null || port == '') {
 app.listen(port, function () {
 console.log('Server running on port: 3000');
 });
+
+// const loginForm = document.getElementById('login-form');
+// const userLoginInput = document.getElementById('user-login');
+// const userPasswordInput = document.getElementById('user-password');
+// const loginBtn = document.querySelector('.login-btn');
 
 // loginForm.addEventListener('submit', function(e) {
 //     //e.preventDefault();
